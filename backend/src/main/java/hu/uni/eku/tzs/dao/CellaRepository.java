7ac0ac89d51aeb3efbe176eb1d8b7ada14e59dd6
Szalóki Dávid <Szunyix@gmail.com>
2020-11-21 16:51:53 +0100

    #11 Minden ami Foglalas (Buildelve)

--- backend/src/main/java/hu/uni/eku/tzs/dao/CellaRepository.java
+++
@@ -1,9 +0,0 @@
-package hu.uni.eku.tzs.dao;
-
-import hu.uni.eku.tzs.dao.entity.Cella;
-import org.springframework.data.repository.CrudRepository;
-
-public interface CellaRepository extends CrudRepository<Cella, Integer>
-{
-    Cella findByCella_id(int cella_id);
-}
