04e3d6e0ed3fa5594b079420b759e1e6428f7892
Maja Bódi <buksi.pajti@gmail.com>
2021-01-14 18:58:42 +0100

    FoglalasModel

--- backend/src/main/java/hu/uni/eku/tzs/model/Foglalas.java
+++ backend/src/main/java/hu/uni/eku/tzs/model/Foglalas.java
@@ -4,7 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.util.Date;
+import java.time.LocalDateTime;
 import java.util.UUID;
 
 @Data
@@ -12,13 +12,13 @@ import java.util.UUID;
 @AllArgsConstructor
 public class Foglalas
 {
-    private UUID Foglalas_Id;
-    private int cellaSzam;
-    private Date erkezes;
-    private Date tavozas;
+    private Integer foglalasId;
+    private Integer cellaSzam;
+    private LocalDateTime erkezes;
+    private LocalDateTime tavozas;
     private String vezeteknev;
     private String keresztnev;
     private String telefonszam;
-    private int tipus;
+    private Integer tipus;
     private boolean aram;
 }
